a0fc197d6bcfe025dfcfc01daf525eb3400761a1
IMACULGY <drewisaacson@live.com>
2019-10-13 13:52:42 -0400

    Modified HelloWorld

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-	std::cout << "Hello World!";
+	int num;
+	num = 6;
+	
+	cout << "My first C++ program." << endl;
+	cout << "The sum of 2 and 3 = " << 5 << endl;
+	cout << "7 + 8 = " << 7 + 8 << endl;
+	cout << "Num = " << num << endl;
+	
+	return 0;
 }
