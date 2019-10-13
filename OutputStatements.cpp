b45dcc84cbe2eca5b4d932e3bdf78ca20b9b39f9
IMACULGY <drewisaacson@live.com>
2019-10-13 13:48:55 -0400

    Added Programs demonstrating Hello World, IOStream, Type Casting

---
+++ OutputStatements.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	a = 65;
+	b = 78;
+	
+	cout << 29 / 4 << endl;
+	cout << 3.0 / 2 << endl;
+	cout << "Hello there.\n";
+	cout << 7 < endl;
+	cout << 3 + 5 << endl;
+	cout << "3 + 5";
+	cout << endl;
+	cout << a << endl;
+	cout << "a" << endl;
+	cout << (a+5)*6 << endl;
+	cout << 2*b << endl;
+	
+	return 0;
+}
